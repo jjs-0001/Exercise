814e28c89261c9f3d0947cc39fe2699825821f12
jjs0001 <jjs-0001@outlook.jp>
2014-05-12 15:32:40 +0900

    Exercise1 clear

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,23 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int i = 100;
+        float f = 50;
+        double d = 50.5;
+        char c = 'a';
+        long l = 200;
+        short s = 50;
+        boolean b = true;
+        byte by = 10;
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(by);
+    }
+}
