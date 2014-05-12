5b722815e3c9cc90b02cb3b50666e45c071798c5
jjs0001 <jjs-0001@outlook.jp>
2014-05-12 16:51:58 +0900

    Q1_10 revise

--- src/jp/co/jjs/java_seminar/self_seminar/q1_10/Q1_10_2.java
+++ src/jp/co/jjs/java_seminar/self_seminar/q1_10/Q1_10_2.java
@@ -2,12 +2,12 @@ package jp.co.jjs.java_seminar.self_seminar.q1_10;
 
 public class Q1_10_2 {
     public static void main(String[] args) {
-        String Myname;
-        String famiryname = "栗田";
-        String name = "英和";
+        String myName;
+        String familyName = "山田";
+        String name = "太一";
 
-        Myname = famiryname + name;
-        System.out.println(Myname);
+        myName = familyName + name;
+        System.out.println(myName);
     }
 
 }
