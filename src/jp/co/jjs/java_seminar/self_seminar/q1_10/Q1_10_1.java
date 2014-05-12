04e5b9d071938360d2c3e77fe3e4f8ba0bcc7853
jjs0001 <jjs-0001@outlook.jp>
2014-05-12 16:45:55 +0900

    Q1_10 clear

---
+++ src/jp/co/jjs/java_seminar/self_seminar/q1_10/Q1_10_1.java
@@ -0,0 +1,9 @@
+package jp.co.jjs.java_seminar.self_seminar.q1_10;
+
+public class Q1_10_1 {
+
+    public static void main(String[] args) {
+        String str = "\"Romeo, where art thou?\"";
+        System.out.println(str);
+    }
+}
---
+++ src/jp/co/jjs/java_seminar/self_seminar/q1_10/Q1_10_2.java
@@ -0,0 +1,13 @@
+package jp.co.jjs.java_seminar.self_seminar.q1_10;
+
+public class Q1_10_2 {
+    public static void main(String[] args) {
+        String Myname;
+        String famiryname = "栗田";
+        String name = "英和";
+
+        Myname = famiryname + name;
+        System.out.println(Myname);
+    }
+
+}
