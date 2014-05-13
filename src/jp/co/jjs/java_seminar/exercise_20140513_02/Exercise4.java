6670cea2d4ae16ed781785cf149173fa2fdabca4
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 10:52:36 +0900

    0513 2nd commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_02/Exercise4.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.exercise_20140513_02;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        short x = 3, y = 10;
+        int z;
+        z = x + y;
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
