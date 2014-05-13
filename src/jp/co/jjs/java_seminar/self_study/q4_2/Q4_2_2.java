40573760b0665bf297affd5142a13ad32595627d
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 17:29:20 +0900

    0513 Q4_2_2 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q4_2/Q4_2_2.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q4_2;
+
+public class Q4_2_2 {
+
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i < 1000;i *= 2){
+            System.out.println(i);
+        }
+    }
+}
