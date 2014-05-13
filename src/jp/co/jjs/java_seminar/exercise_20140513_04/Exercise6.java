3888f50931a744b149059206d41475b4a6c407a9
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 11:58:38 +0900

    0513 4th commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_04/Exercise6.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.exercise_20140513_04;
+
+public class Exercise6 {
+
+    public static void main(String[] args) {
+        int n = 1;
+
+        if(n % 2 == 1){
+            System.out.println(n +"は奇数です");
+        }else{
+            System.out.println(n +"は偶数です");
+        }
+    }
+}
