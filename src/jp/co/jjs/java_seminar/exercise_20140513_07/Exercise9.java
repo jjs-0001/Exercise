6e4c5a7b95526bc01b18b3acb5bbe7527cf084fd
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 14:44:39 +0900

    0513 7th commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_07/Exercise9.java
@@ -0,0 +1,22 @@
+package jp.co.jjs.java_seminar.exercise_20140513_07;
+
+public class Exercise9 {
+
+    public static void main(String[] args) {
+        int[] x = new int[4];
+        int[] y = {1, 2, 3, 4};
+
+        String[] a = new String[3];
+        String[] b = {"String", "Int"};
+
+        x[0] = 50;
+        a[0] = "char";
+
+        System.out.println(y.length);
+        System.out.println(y[2]);
+        System.out.println(x[0]);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(a[0]);
+    }
+}
