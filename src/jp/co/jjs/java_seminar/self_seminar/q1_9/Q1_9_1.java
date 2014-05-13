ffd3df34fe65ecb03ae03758d8e3af4b922dc056
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 10:19:39 +0900

    deleted

--- src/jp/co/jjs/java_seminar/self_seminar/q1_9/Q1_9_1.java
+++
@@ -1,9 +0,0 @@
-package jp.co.jjs.java_seminar.self_seminar.q1_9;
-
-public class Q1_9_1 {
-
-    public static void main(String[] args) {
-        int num = 1000;
-        System.out.println(num + "is the value of num");
-    }
-}
