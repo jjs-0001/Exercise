1b30bdabaa65c97d9a9ffc1d0a5174400e786ac2
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 17:19:30 +0900

    0513 Q4_2_1 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q4_2/Q4_2_1.java
@@ -0,0 +1,15 @@
+package jp.co.jjs.java_seminar.self_study.q4_2;
+
+public class Q4_2_1 {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        for( ; n != 1; n--){
+
+        }
+        if(n == 1){
+            System.out.println('\u0007');
+        }
+    }
+}
