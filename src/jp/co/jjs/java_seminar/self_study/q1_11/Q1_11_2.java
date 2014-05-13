d7418827c8a989bb41dd54672269c475368a0131
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 10:36:48 +0900

    0513 self_study_q1_11_2

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_11/Q1_11_2.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q1_11;
+
+public class Q1_11_2 {
+
+    public static void main(String[] args) {
+        int road = 3000;
+        int speed = 75;
+
+        System.out.println("移動時間は"+ road / speed +"時間です。");
+    }
+}
