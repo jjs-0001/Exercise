36bdedcf9e656888d18964da58c7fbabf8d05c0d
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 11:10:02 +0900

    0513 3rd commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_03/Exercise5.java
@@ -0,0 +1,16 @@
+package jp.co.jjs.java_seminar.exercise_20140513_03;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        byte b;
+        int x = 127;
+
+        b = (byte)x;
+        System.out.println(b);
+
+        x = 258;
+        b = (byte)x;
+        System.out.println(b);
+    }
+}
