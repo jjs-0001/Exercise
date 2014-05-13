7d041e739827a85afc0ef40fb35288b15b4ce81d
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 14:15:47 +0900

    0513 self_study_Q1_13

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_13/Q1_13_1.java
@@ -0,0 +1,10 @@
+package jp.co.jjs.java_seminar.self_study.q1_13;
+
+public class Q1_13_1 {
+
+    public static void main(String[] args) {
+        short s1 = 1;
+        //short s2 = s1 + 1;
+        //System.out.println(s2);
+    }
+}
