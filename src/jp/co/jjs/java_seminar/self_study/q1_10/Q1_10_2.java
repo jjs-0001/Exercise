c23a01c029c06f679d674f4fa52c5c7781eab33a
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 10:16:07 +0900

    name study

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/Q1_10_2.java
@@ -0,0 +1,13 @@
+package jp.co.jjs.java_seminar.self_study.q1_10;
+
+public class Q1_10_2 {
+    public static void main(String[] args) {
+        String myName;
+        String familyName = "山田";
+        String name = "太一";
+
+        myName = familyName + name;
+        System.out.println(myName);
+    }
+
+}
---
+++ src/jp/co/jjs/java_seminar/self_study/q1_9/Q1_9_2_src.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q1_9;
+
+public class Q1_9_2_src {
+    public static void main(String args[]) {
+        char c;
+        boolean flag;
+        //System.out.println(c);
+        //System.out.println(flag);
+    }
+
+}
