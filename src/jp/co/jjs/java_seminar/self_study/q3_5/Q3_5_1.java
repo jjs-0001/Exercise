f1b30e6952214f4b944329117ca8b17809bada4d
jjs0001 <jjs-0001@outlook.jp>
2014-05-13 16:59:02 +0900

    0513 Q3_5_1 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q3_5/Q3_5_1.java
@@ -0,0 +1,10 @@
+package jp.co.jjs.java_seminar.self_study.q3_5;
+
+public class Q3_5_1 {
+
+    public static void main(String[] args) {
+        for(int i = 20; i <= 30; i++){
+            System.out.println(i);
+        }
+    }
+}
