6bf80d42fa706992266e9863e0a58e596646b0ae
jjs0001 <jjs-0001@outlook.jp>
2014-05-14 12:44:40 +0900

    0514 Q3_7 commit]

---
+++ src/jp/co/jjs/java_seminar/self_study/q3_7/Q3_7.java
@@ -0,0 +1,18 @@
+package jp.co.jjs.java_seminar.self_study.q3_7;
+
+public class Q3_7 {
+
+    public static void main(String[] args) {
+        for(int x = 0; x < 100; x++){
+            System.out.println(x); // Q3_7_1 0～99までを表示する
+        }
+
+        if(!(10 == 9)){
+            System.out.println("真実である"); //Q3_7_2 真実である
+        }
+
+        // if((a >= 0) || (c > b / c))  Q3_7_3 aが0未満のとき
+    }
+
+
+}
