149203000c64c6a0fb95a05db3ea2d51622ea0d7
jjs0001 <jjs-0001@outlook.jp>
2014-05-14 10:53:04 +0900

    0514 1st commit

---
+++ src/jp/co/jjs/java_seminar/exercise_20140514_01/Book.java
@@ -0,0 +1,18 @@
+package jp.co.jjs.java_seminar.exercise_20140514_01;
+
+public class Book {
+
+    int id; //本のID
+    String Title; //本のタイトル
+    String writer; //本の著者
+    String publisher; //本の出版社
+    boolean lend; //本の貸し出し状況
+
+    public void out(){
+        lend = false;
+    }
+
+    public void in(){
+        lend = true;
+    }
+}
