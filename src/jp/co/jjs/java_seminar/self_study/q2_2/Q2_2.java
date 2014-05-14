d9c6c0ebfa9e9ff5d3a3a01525b7fd428b576b03
jjs0001 <jjs-0001@outlook.jp>
2014-05-14 17:20:01 +0900

    0514 Q2_2 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q2_2/Q2_2.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.self_study.q2_2;
+
+public class Q2_2 {
+
+    public static void main(String[] args) {
+        double d = Math.sqrt(23.45);
+
+        System.out.println(d);
+        System.out.println(Math.pow(2, 12));
+
+        double b = Math.hypot(4.5, 8.9);
+        System.out.println(b);
+    }
+}
