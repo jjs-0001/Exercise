45d66ded865c9bd1467d963bdd23c3d6919709ff
jjs0001 <jjs-0001@outlook.jp>
2014-05-14 09:33:12 +0900

    0514 Q1_14 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_14/Q1_14.java
@@ -0,0 +1,12 @@
+package jp.co.jjs.java_seminar.self_study.q1_14;
+
+public class Q1_14 {
+
+    public static void main(String[] args) {
+
+        /**/  //コメントは書かないと意味が無い
+        /*System.out.println("this is a test");*/   //基本的にはプログラムをコメントはしない
+
+        // これはコメントです。
+    }
+}
