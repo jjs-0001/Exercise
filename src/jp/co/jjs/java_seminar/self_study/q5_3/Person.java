b1fdf0de4040bc8bf41e60ab2bc2b849eef5234b
jjs0001 <jjs-0001@outlook.jp>
2014-05-15 12:40:05 +0900

    0515 Q5_3 commit

---
+++ src/jp/co/jjs/java_seminar/self_study/q5_3/Person.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.self_study.q5_3;
+
+public class Person {
+
+    String name;
+    int age;
+    float income;
+
+    public Person(String n, int a, float in) {
+        this.name = n;
+        this.age = a;
+        this.income = in;
+    }
+}
---
+++ src/jp/co/jjs/java_seminar/self_study/q5_3/Sphere.java
@@ -0,0 +1,14 @@
+package jp.co.jjs.java_seminar.self_study.q5_3;
+
+public class Sphere {
+
+    double line;
+    double x;
+    double y;
+
+    public Sphere(double l, double ax, double ay) {
+        this.line = l;
+        this.x = ax;
+        this.y = ay;
+    }
+}
